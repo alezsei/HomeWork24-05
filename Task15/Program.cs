fde2ef196c127cb9d5271041414ac8bcb6f91769
alezsei <alezsei@yandex.ru>
2023-05-24 21:30:34 +0400

    task15

---
+++ Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿int InText(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Task15()
+{
+    int numday = InText("Введите день недели");
+    if (numday > 0 && numday < 8)
+    {
+        if (numday == 6 || numday == 7)
+        {
+            Console.WriteLine("Выходной");
+        }
+        else Console.WriteLine("Не выходной");
+    }
+    else Console.WriteLine("Нет такого дня недели");
+
+}
+Task15();
