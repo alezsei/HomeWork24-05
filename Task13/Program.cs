0655367b8ef2fe2e1402caa28b936a893486780b
alezsei <alezsei@yandex.ru>
2023-05-24 21:16:08 +0400

    task13

---
+++ Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿int IntNum(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void Task13()
+{
+    int num1 = IntNum("Введите число");
+    int threenum = num1;
+    if (threenum > 99)
+    {
+        while (threenum > 999)
+        {
+            threenum = threenum / 10;
+        }
+        Console.WriteLine($"Третья цифра числа {num1} равна {threenum % 10} "); 
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры числа нет");
+    }
+}
+Task13();
